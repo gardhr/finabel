ddd5063502ad7352e96ca1822ea8f4bcb5f4d238
gardhr <24468810+gardhr@users.noreply.github.com>
2020-11-18 22:03:49 -0600

    fix text conversion anomalies

---
+++ cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const finabel = require("./finabel");
+let argv = process.argv.slice(1);
+let argc = argv.length;
+console.log("Usage:", argv[0], "[KEY] [SALT] [ROUNDS] [DIGITS]");
+let key = "";
+if (argc > 1) key = argv[1];
+let salt = "";
+if (argc > 2) key = argv[2];
+let rounds = 0;
+if (argc > 3) rounds = atoi(argv[3]);
+let digits = 0;
+if (argc > 4) digits = atoi(argv[4]);
+console.log(finabel(key, salt, rounds, digits, true));
