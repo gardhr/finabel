eca0a3886cff402935701f7ed212eaf8b804a240
gardhr <24468810+gardhr@users.noreply.github.com>
2020-11-24 18:35:45 -0600

    sandbox

--- sandbox-cli.js
+++ sandbox-cli.js
@@ -1,9 +1,9 @@
 const finabel = require("./sandbox");
 let argv = process.argv.slice(1);
 let argc = argv.length;
-console.log("Usage:", argv[0], "[KEY] [SALT] [ROUNDS] [DIGITS] [EXPENSE]");
-let key = "";
-if (argc > 1) key = argv[1];
+console.log("Usage:", argv[0], "[PASSWORD] [SALT] [ROUNDS] [DIGITS] [EXPENSE]");
+let password = "";
+if (argc > 1) password = argv[1];
 let salt = "";
 if (argc > 2) salt = argv[2];
 let rounds = 0;
@@ -14,7 +14,7 @@ let expense = 0;
 if (argc > 5) expense = Number(argv[5]);
 console.log(
   finabel({
-    key: key,
+    password: password,
     salt: salt,
     rounds: rounds,
     digits: digits,
